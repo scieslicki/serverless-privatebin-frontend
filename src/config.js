d716d0e442b7133aa2ffd1f49048d90430ab9479
Staszek Cieslicki <s.cieslicki@clearcode.cc>
2020-04-17 11:29:15 +0200

    fix api endpoints

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const dev = {
   defaultPassword: "Sp4DSezu2HhLctKvhaWFGwObvMhaLTji",
   apiGateway: {
     REGION: "eu-central-1",
-    URL: "https://8pqtao81vd.execute-api.eu-central-1.amazonaws.com/dev"
-    // URL: "https://dev.api.dobrastronka.pl/privatebin"
+    // URL: "https://8pqtao81vd.execute-api.eu-central-1.amazonaws.com/dev"
+    URL: "https://dev.api.dobrastronka.pl/api/privatebin"
   },
 };
 
@@ -11,8 +11,8 @@ const prod = {
   defaultPassword: "XYTZMHPV83ogYjthNijZwPFHqJJyQpug",
   apiGateway: {
     REGION: "eu-central-1",
-    URL: "https://8pqtao81vd.execute-api.eu-central-1.amazonaws.com/dev"
-    // URL: "https://api.dobrastronka.pl/privatebin"
+    // URL: "https://8pqtao81vd.execute-api.eu-central-1.amazonaws.com/dev"
+    URL: "https://dev.api.dobrastronka.pl/api/privatebin"
   },
 };
 
